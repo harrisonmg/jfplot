20ae6d0151e6cfe420d6f38d96b1991e309bcb37
Harrison MG <gieraltowski.har@gmail.com>
2022-10-30 01:14:10 -0400

    improved project structure a bit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-   static: './dist'
+    static: './dist',
+    watchFiles: ["src/**/*"]
   },
   entry: './src/main.js',
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'ify-loader'
-  }]},
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'ify-loader'
+      },
+      {
+        test: /\.html$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/index.html' },
+        { from: 'src/main.css' },
+      ]
+    })
+  ]
 };
