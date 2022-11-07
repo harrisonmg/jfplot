847e555dcaa457a59809e262bff7213cdf8988f1
Harrison MG <gieraltowski.har@gmail.com>
2022-11-07 11:23:44 -0500

    multi file / multi series progress

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Plotly from 'plotly.js-dist';
 import * as dfd from 'danfojs';
 
+const dfs = {};
+
 const layout = {
   title: ''
 };
@@ -14,27 +16,58 @@ const plot_options = {
   responsive: true
 };
 
-Plotly.newPlot('plot', [], layout, plot_options);
+const updateSelect = (select, options) => {
+  console.log(select);
+  while (select.firstChild) {
+    select.removeChild(select.firstChild);
+  }
 
-document.getElementById('btn')
-  .addEventListener('click', () => {
-  });
+  for (const label in options) {
+    const option = document.createElement('option');
+    option.value = label;
+    option.innerText = label;
+    select.appendChild(option);
+  }
+};
 
-const fileList = document.getElementById('file-list');
+const addSeries = () =>
+{
+  const template = document.querySelector('#series-template');
+  const series = template.content.cloneNode(true);
+  // TODO add event listener for file select to populate x and y here
+  updateSeries(series);
+  document.querySelector('body').appendChild(series);
+};
+
+const updateSeries = (series) =>
+{
+  let fileSelect = series.querySelector('.file-select');
+  updateSelect(fileSelect, dfs);
+};
+
+document.getElementById('add-series-button')
+ .addEventListener('click', () => {
+   addSeries();
+ });
 
 document.getElementById('file-input')
   .addEventListener('change', (event) => {
     const filename = event.target.value.split('\\').slice(-1);
-    const li = document.createElement('li');
-    li.innerText = filename;
-    fileList.appendChild(li);
     dfd.readCSV('./' + filename).then((df) => {
       df = dfd.toJSON(df, { format: 'row' });
-      const traces = [{
-        x: df['time'],
-        y: df['current_distance'],
-        mode: 'markers'
-      }]
-      Plotly.react('plot', traces);
+      dfs[filename] = df;
+      for (const series of document.querySelectorAll('.series')) {
+        updateSeries(series);
+      }
+
+      //const traces = [{
+      //  x: df['time'],
+      //  y: df['current_distance'],
+      //  mode: 'markers'
+      //}]
+      //Plotly.react('plot', traces);
     })
   });
+
+Plotly.newPlot('plot', [], layout, plot_options);
+addSeries();
